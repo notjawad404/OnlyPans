d3fea844bea90ae700f90530b0d6fe255672b35e
Jawad Ali <jawadali000123@gmail.com>
2024-08-04 00:55:55 +0500

    Add Routes and DB connection

--- Backend/controller/recipeController.js
+++ Backend/controller/recipeController.js
@@ -1,6 +1,22 @@
 
 const getRecipes = (req, res) =>{
-    res.json({message: 'Recipes'})
+    res.json({message: 'Get all Recipes'})
 }
 
-module.exports = {getRecipes}
+const getRecipe = (req, res) =>{
+    res.json({message: 'Get Recipe by ID'})
+}
+
+const addRecipe = (req, res) =>{
+    res.json({message: 'Add a Recipe'})
+}
+
+const editRecipe = (req, res) =>{
+    res.json({message: 'Edit a Recipe'})
+}
+
+const deleteRecipe = (req, res) =>{
+    res.json({message: 'Delete a Recipe'})
+}
+
+module.exports = {getRecipes, getRecipe, addRecipe, editRecipe, deleteRecipe}
--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/connectionDB.js');
 
+dotenv.config();
 const app = express();
+ 
+const PORT = process.env.PORT || 5002;
 
-const PORT = 5002;
-
-app.use('/recipe', require('./routes/recipe.js'));
+connectDB(); 
+app.use('/recipe', require('./routes/recipeRoutes.js'));
 
 app.listen(PORT, (err) => {
     console.log("Server listening at "+ PORT);
